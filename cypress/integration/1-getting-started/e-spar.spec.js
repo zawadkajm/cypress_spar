7fa625326f02f8f12bf361aecfb14ea209b0437a
Konrad <konrad@soft4pm.com>
2021-10-18 23:21:20 +0200

    test development

--- cypress/integration/1-getting-started/e-spar.spec.js
+++ cypress/integration/1-getting-started/e-spar.spec.js
@@ -10,9 +10,25 @@ describe('test e-spar', () => {
         //obsługa drugiego pop-upu
         cy.get('#delivery-complete').click()
         //wyszukiwanie produktu
+        // 1
         cy.get('#query', {timeout: 10000}).should('have.length', 1).should('be.visible').type('mleko{enter}')
-        // cy.get('#query', {timeout: 8000})
-        // cy.get('#query', {timeout: 8000}).type('mleko')
+        cy.get('.product-form-290982 > .product-box__buttons > .basket > .add-to-cart').should('be.visible').click()
+        // 2
+        cy.get('#query').should('be.visible').clear()
+        cy.get('#query', {timeout: 10000}).should('have.length', 1).should('be.visible').type('ser{enter}')
+        cy.get('.product-form-221159 > .product-box__buttons > .basket > .add-to-cart').should('be.visible').click()
+        cy.get('.product-form-221159 > .product-box__buttons > .product-edit > .product-unit > .unit-sl').should('be.visible').click()
+        cy.get('.product-form-221159 > .product-box__buttons > .product-edit > .product_quantity__wrapper > .product-amount-input').type('{selectall}3')
+        // przejscie do koszyka
+        cy.get('.top-search__right > .open-cart').should('be.visible').click()
+        cy.get('#finalize-shopping').should('be.visible').click()
+        cy.get('.buttons > .total-price3 > .btn').should('be.visible').click()
+        // dane adresowe
+        cy.get('#delivery > :nth-child(2) > label').should('be.visible').click()
+        cy.get('.col-lg-9 > :nth-child(6) > label').should('be.visible').click()
+        cy.get('#store_models_StoreClientAddress_firstname').type('Urząd Gminy')
+        cy.get('#store_models_StoreClientAddress_lastname').type('Michałowice')
+        
     })
 
 })
